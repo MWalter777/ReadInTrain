744ba2982901ef895ae8369549a19af224b2ce11
MWalter777 <waltermarrom777@gmail.com>
2020-10-17 13:07:55 -0600

    Proyecto finalizado

--- ReadInTrain/Program.cs
+++ ReadInTrain/Program.cs
@@ -12,16 +12,17 @@ namespace ReadInTrain
 
         static void Main(string[] args)
         {
+            int hoursTotal = 0;
+            Console.WriteLine("Digite el numero de horas a permanecer en el tren");
             int hourTrain = readNumberBetween(0,1000);
+            Console.WriteLine("Digite el numero de horas a leer");
             int hourReader = readNumberBetween(0, hourTrain);
+            Console.WriteLine("Digite el numero minimo de luz que necesita para leer");
             int levelLightMin = readNumberBetween(30, 70);
-            travel = new Travel(hourTrain, hourReader, levelLightMin, levelLightMin + 30);
+            travel = new Travel(hourTrain, hourReader, levelLightMin, 95);
 
-            for(int i = 0; i < hourTrain; i++)
-            {
-                int number = readNumberBetween(0, 100);
-                travel.addToLevel(new LevelLight(number, i + 1));
-            }
+            Console.WriteLine("Digite el nivel de luz durante cada hora en el tren separados por espacios");
+            readNumberBetween();
 
             Console.Clear();
 
@@ -30,11 +31,60 @@ namespace ReadInTrain
                 if (l.canRead(travel.levelMinForRead, travel.levelMaxForRead))
                 {
                     Console.Write(" {0} : {1},  ", l.hour, l.level);
+                    hoursTotal++;
                 }
+            }
+
+            if (hoursTotal >= hourReader)
+            {
+                Console.WriteLine("Puedes leer las horas que seleccionaste, en total tienes {0} horas disponibles", hoursTotal);
+            }else
+            {
+                Console.WriteLine("No puedes leer todo el tiempo establecido, solo tienes {0} horas disponibles", hoursTotal);
             }
 
             Console.ReadKey();
 
+        }
+
+        private static void readNumberBetween()
+        {
+            int hourTrain = travel.hourTrain;
+            int salir = 0;
+            do
+            {
+                string levelForHours = Console.ReadLine();
+                string[] levels = levelForHours.Split(' ');
+                Console.Write("length = {0}", levels.Length);
+                if (levels.Length != hourTrain)
+                {
+                    Console.WriteLine("Error debe ingresar el nivel de luz de todas las horas ({0}) y solo numeros separados por espacios", hourTrain);
+                }
+                else
+                {
+                    try
+                    {
+                        List<LevelLight> level = new List<LevelLight>();
+                        for(int i=0; i<levels.Length; i++)
+                        {
+                            int n = int.Parse(levels[i]);
+                            if(n>=0 && n <= 100)
+                            {
+                                level.Add(new LevelLight(n, i + 1));
+                                salir++;
+                            }
+                        }
+                        if(salir == hourTrain)
+                        {
+                            salir = 1;
+                            travel.levels = level;
+                        }
+                    } catch(Exception e)
+                    {
+                        salir = 0;
+                    }
+                }
+            } while (salir <=0);
         }
 
         private static int readNumberBetween(int v1, int v2)
